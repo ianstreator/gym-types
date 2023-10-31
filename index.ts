cea829153693895388993481592552bdbccbe453
ian <ianstreator+github@gmail.com>
2023-10-30 20:33:15 -0500

    updated types and version

--- index.ts
+++ index.ts
@@ -1,26 +1,51 @@
 export namespace Types {
-  type Activity = {
+  export type Activity = {
     name: string;
     setsAmount: number;
     repsAmount: number;
     restSeconds: number;
   };
+  export type Workout = {
+    date: Date;
+    _id: string;
+    completion: number;
+    activies?: Activity[];
+  };
 
-  type User = {
+  export type User = {
     email: string;
+    setEmail?: (userEmail: string) => void;
     username: string;
+    setUsername?: (name: string) => void;
     schedule: {
-      sunday?: Activity[];
-      monday?: Activity[];
-      tuesday?: Activity[];
-      wednesday?: Activity[];
-      thursday?: Activity[];
-      friday?: Activity[];
-      saturday?: Activity[];
+      sunday: Activity[];
+      monday: Activity[];
+      tuesday: Activity[];
+      wednesday: Activity[];
+      thursday: Activity[];
+      friday: Activity[];
+      saturday: Activity[];
     };
-    workouts: [{ date: Date; activies: Activity[]; completion: number }];
-    verified: boolean;
-    verificationCode: string;
-    authentication: { salt?: string; password?: string; sessionToken?: string };
+    setSchedule?: (day: string, activities: Activity[]) => void;
+    workouts?: Workout[];
+    setWorkouts?: ({
+      date,
+      activities,
+      completion,
+    }: {
+      date: Date;
+      activities: Activity[];
+      completion: number;
+    }) => void;
+    verified?: boolean;
+    verificationCode?: string;
+    _id: string;
+    setId?: (id: string) => void;
+    authentication?: {
+      salt?: string;
+      password?: string;
+      sessionToken?: string;
+    };
+    setState?: (userFields: User) => void;
   };
 }
